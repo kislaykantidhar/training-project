4968527e3ce2a89c6b45a7dd7bf0e377bd04f506
Kislay Kanti Dhar <kislay.dhar@bitcanny.com>
2020-08-18 18:14:49 +0530

    PROJECT FILES

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const signupAdmin=require('./routes/signupAdmin');
 const loginAdmin=require('./routes/loginAdmin');
 const signupUser=require('./routes/signupUser');
 const loginUser=require('./routes/loginUser');
-const createTask=require('./routes/createTask')
-
+const createTask=require('./routes/createTask');
+const viewTask=require('./routes/viewTasks');
+const comment=require('./routes/comment')
 
 app.use(helmet());
 app.use(express.json());
@@ -17,6 +18,8 @@ app.use('/',loginAdmin);
 app.use('/',signupUser);
 app.use('/',loginUser);
 app.use('/',createTask);
+app.use('/',viewTask);
+app.use('/',comment);
 
 app.listen(5227);
 console.log('Server started at 127.0.0.1:5227');

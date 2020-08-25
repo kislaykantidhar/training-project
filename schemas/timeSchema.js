5a971a5fed0316669c15764bf6eef2c0581f4712
Kislay Kanti Dhar <kislay.dhar@bitcanny.com>
2020-08-25 20:52:42 +0530

    schemas

--- schemas/timeSchema.js
+++ schemas/timeSchema.js
@@ -3,7 +3,8 @@ const Joi=require('joi');
 
 const timeSchema=Joi.object({
     startedAt:Joi.string().pattern(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/).required(),
-    endedAt:Joi.string().pattern(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/).required()
+    endedAt:Joi.string().pattern(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/).required(),
+    summary:Joi.string().min(1)
 })
 
 module.exports={timeSchema:timeSchema}
